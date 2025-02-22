fc329fb6c1420da9b301aaa4af8f8aca9685e824
Penk0vXd <stanimir.penkov19@gmail.com>
2025-02-22 21:58:05 +0200

    Добавяне на source кода

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿namespace Rock_Paper_Scisor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("За да започнеш трябва да въведеш камък, ножица, хартия");
+
+            string type = Console.ReadLine().ToLower();
+
+            Random random = new Random();
+            int n = random.Next(1, 4);
+            string simvol = "";
+
+            int PC = 0;
+            int AI = 0;
+
+            switch (n)
+            {
+                case 1:
+                    simvol = "камък";
+                    break;
+                case 2:
+                    simvol = "ножица";
+                    break;
+                case 3:
+                    simvol = "хартия";
+                    break;
+            }
+
+            if (type != "камък" && type != "ножица" && type != "хартия")
+            {
+                Console.WriteLine("Въведи валиден символ (Камък,Ножица,Хартия)");
+            }
+
+            else if (type == "камък" && simvol == "хартия" || type == "ножица" && simvol == "камък" || type == "хартия" && simvol == "ножица")
+            {
+                Console.WriteLine(simvol);
+                Console.WriteLine("Ти загуби");
+                AI++;
+                Console.WriteLine($"Ти: {PC}, AI: {AI}");
+            }
+
+            else if (type == "камък" && simvol == "ножица" || type == "ножица" && simvol == "хартия" || type == "хартия" && simvol == "камък")
+            {
+                Console.WriteLine(simvol);
+                Console.WriteLine("Ти спечели");
+                PC++;
+                Console.WriteLine($"Ти: {PC}, AI: {AI}");
+            }
+
+            else if (type == "камък" && simvol == "камък" || type == "ножица" && simvol == "ножица" || type == "хартия" && simvol == "хартия")
+            {
+                Console.WriteLine(simvol);
+                Console.WriteLine("Равенство");
+                Console.WriteLine($"Ти: {PC}, AI: {AI}");
+            }
+        }
+    }
+}
